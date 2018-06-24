937ec95e99211030daf2c41afa565f235a5edf2b
jormono <frogjmon@yahoo.com>
2018-06-24 08:10:14 -0400

    Update reddit_email_bot.py
    I changed the search criteria from being a list of subreddits and a list of words/phrases to a dictionary. This should allow the user set different keywords/phrases per subreddit. 
    Also some minor bug fixes in the e-mail portion of the script to handle for the maximum character length and a bug that was coming up with certain characters.
    
    Ran this for a while this morning, works well except occasionally the encode doesn't know a character and yields "???", and I also got 2 or 3 blank messages (out of probably 200), not sure what that is about.

--- reddit_email_bot.py
+++ reddit_email_bot.py
@@ -2,18 +2,17 @@ import praw
 import time
 import smtplib
 from credentials import reddit_client_id, reddit_client_secret, reddit_password, reddit_user_agent, reddit_username, email_bot_address, email_bot_password, email_user_address
-
-'''Reddit bot portion from r/RequestABot authored by /u/John_Yuki.
-Modification for sending e-mail by /u/jormono'''
+'''Reddit Bot Portion Based at r/RequestABot authored by /u/John_Yuki.
+Modification for sending e-mail and for criteria by dictionary by /u/jormono'''
 
 # The keywords or phrases that the bot looks for.
-# Keywords and phrases are NOT case sensitive. However it will match them literally, so make sure you spell them correctly.
-keywords = ['test', 'example', 'another example']
+# changed from 2 lists to one dictionary, this should let you set search value per subreddit
+criteria = {
+    'news': ['o'],
+    'politics': ['i', 'a', 'u'],
+    'AskReddit': ['e']}
 
-# You can have 1 subreddit or multiple. If you have multiple, separate them with + symbols like I have done in the example.
-# Also, you do not need to include the "/r/" part, just the name of the subreddit as shown in the example.
-# The subreddit can also just be "all" if you want it to search through every subreddit in existence. (Might get spammy if you have a common or a lot of keywords).
-subreddits = 'news+askreddit'
+criteria_keys = '+'.join(criteria.keys())
 
 try:
     reddit = praw.Reddit(username = reddit_username,
@@ -21,36 +20,41 @@ try:
         client_secret = reddit_client_secret, user_agent = reddit_user_agent)
 except Exception as e:
     print(e)
+    print('Reddit connection issue')
     time.sleep(60)
 
 # Main program:
 def send_message(submission, reddit):
     try:
         # e-mail message
-        msg = 'From: {}\nTo: {}\n{} \n {}'.format(email_bot_address, email_user_address, submission.title, submission.shortlink)
-        mail = smtplib.SMTP('smtp.gmail.com', 587) # setup for gmail, if not using gmail will need to change this
+        post_title = submission.title
+        if len(str(submission.title))>137: # message character limit minus length of url
+            post_title = str(post_title)[:130] + '...' # if title exceeds character limit for message this trims it up
+        msg = 'From: {}\nTo: {}\n{} \n {}'.format(email_bot_address, email_user_address, post_title, submission.shortlink)
+        mail = smtplib.SMTP('smtp.gmail.com', 587) # specific to gmail
         mail.ehlo()
         mail.starttls()
         mail.login(email_bot_address, email_bot_password)
-        mail.sendmail(email_bot_address, email_user_address, msg)
+        mail.sendmail(email_bot_address, email_user_address, msg.encode('utf-8')) # added encode uf-8 due to error that kept coming up
         mail.quit()
         print('Sent new message') # This line is for testing purposes.
     except Exception as e:
         print(e)
+        print('email issue')
         time.sleep(60)
-        
         
 def find_submissions():
     try:
         while True:
             start_time = time.time()
-            for submission in reddit.subreddit(subreddits).stream.submissions():
-                for keyword in keywords:
+            for submission in reddit.subreddit(criteria_keys).stream.submissions():
+                for keyword in criteria[str(reddit.subreddit(str(submission.subreddit)))]:
                     if keyword.lower() in submission.title.lower() and submission.created_utc > start_time:
                         send_message(submission, reddit)
                         break
     except Exception as e:
         print(e)
+        print('Reddit search issue')
         time.sleep(60)
 
 if __name__ == '__main__':
